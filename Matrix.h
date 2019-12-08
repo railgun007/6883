e9c1815f58b9ebb643cda0de31de04121cedbee3
Blizzardoo <48665173+Blizzardoo@users.noreply.github.com>
2019-12-08 13:33:56 -0500

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -12,6 +12,7 @@ Vector operator*(const Matrix& C,const Vector& V);
 Vector operator*(const double& a,const Vector& V);
 Vector operator+(const double& a,const Vector& V);
 Vector operator+(const Vector& V,const Vector& W);
+Vector operator-(const Vector& V,const Vector& W);
 Vector operator/(const Vector& V,const Vector& W);
 Vector exp(const Vector& V);
 double operator^(const Vector& V,const Vector& W);
